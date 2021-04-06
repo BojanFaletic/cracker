3c46c87db0a066d974e0e118cd663397c54b710c
ZanPajntar <zan.pajntar@gmail.com>
2021-04-06 18:55:04 +0200

    with matic's timer

--- avr/main.cpp
+++ avr/main.cpp
@@ -1,2 +1,50 @@
 #include <avr/io.h>
-int main(){}
+#include <util/delay.h>
+
+int main ()
+{
+	DDRB |= (1 << PB0);
+
+	while(1) 
+	{
+		PORTB ^= (1 << PB0);
+		_delay_us(1000);
+		_delay_ms(1000);
+		/*_delay_ms(1000);
+		_delay_ms(1000);
+		_delay_ms(1000);
+		_delay_ms(1000);
+		_delay_ms(1000);*/
+	}
+}
+
+namespace INT {
+static uint16_t noOfTicks;
+void stop();
+
+void startTimer1() {
+  // Set counter value to 0
+  TCNT1 = 0x00;
+  // Reset TCCR1A options since Arduino likes to enable some of them.
+  TCCR1A = 0x00;
+  // Set timer to normal mode and set prescaler to 1.
+  TCCR1B = 0x01;
+}
+
+void stopTimer1() { TCCR1B = 0x00; }
+
+uint16_t getTimerTicks() { return TCNT1; }
+
+void start() {
+  startTimer1();
+  while (digitalRead(HW::RX_1))
+    ;
+  stop();
+}
+
+void stop() {
+  stopTimer1();
+  noOfTicks = getTimerTicks();
+}
+
+} // namespace INT
